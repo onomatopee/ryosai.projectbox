a106b3b7ff0882fdbe14f3021ee8dffe960291b6
Shinnosuke Shoda <raindrops@vivaldi.net>
2024-10-15 06:00:28 +0900

    update output system

--- js/main.js
+++ js/main.js
@@ -1,22 +1,147 @@
-const time_put = document.getElementById('timePutter');
-const place_put = document.getElementById('placePutter');
-const text_put = document.getElementById('textPutter');
-
-document.querySelector('button').addEventListener('click', () => {
-  const time_get = document.getElementById('timeGetter');
-  const place_get = document.getElementById('placeGetter');
-  const text_get = document.getElementById('textGetter');
-
-  time_put.textContent = time_get.value;
-  place_put.textContent = place_get.value;
-  text_put.textContent = text_get.value;
+document.addEventListener("DOMContentLoaded", () => {
+  const $preview = document.querySelector("#result");
+  $preview.addEventListener("input", () => {
+    for (const $img of $preview.querySelectorAll("img")) {
+      if ($img.src.match(/^https*:/)) {
+        $img.crossOrigin = "anonymous";
+      }
+    }
+  }, false);
 });
 
-document.querySelector('#output').addEventListener('click', () => {
-  html2canvas(document.querySelector("#result")).then(canvas => {
-    document.body.appendChild(canvas)
+
+function update() {
+  document.getElementById('timePutter').textContent = document.getElementById('timeGetter').value;
+  document.getElementById('placePutter').textContent = document.getElementById('placeGetter').value;
+  document.getElementById('textPutter').textContent = document.getElementById('textGetter').value;
+};
+
+//const target = document.querySelector("#result");
+// document.querySelector('#output').addEventListener('click', () => {
+//   start()
+// });
+
+
+async function start() {
+  const $css = document.querySelector("#css");
+  const $preview = document.querySelector("#result");
+  const $canvas = document.querySelector("#canvas");
+  const ctx = $canvas.getContext("2d");
+
+  const clientRect = $preview.getBoundingClientRect();
+  const w = clientRect.width;
+  const h = clientRect.height;
+
+  const cssText = await replaceUrlToBase64($css.innerHTML);
+
+  const svgText = elementToSvgText($preview, cssText);
+
+  const svgBase64 =
+    "data:image/svg+xml;base64,"
+    + btoa(decodeURI(encodeURIComponent(svgText)));
+  
+  // 2倍のサイズでcanvasに描画する
+  // svgなので描画サイズは自由に変えられる
+  $canvas.width = w * 2;
+  $canvas.height = h * 2;
+
+  const img = new Image();
+  img.src = svgBase64;
+
+  img.onload = () => {
+    // 2倍のサイズでcanvasに描画する
+    ctx.drawImage(img, 0, 0, w * 2, h * 2);
+  }
+  img.onerror = err => {
+    console.log(err);
+  }
+}
+
+function saveCanvas() {
+  const $canvas = document.querySelector("#canvas");
+  const $a = document.createElement("a");
+  $a.download = "canvasTest.png";
+  document.body.append($a);
+  console.log($canvas.toDataURL());
+  $canvas.toBlob(blob => {
+    $a.href = URL.createObjectURL(blob);
+    $a.click();
+    URL.revokeObjectURL(blob);
+    $a.remove();
+  }, "image/png");
+}
+
+// CSS内のurlをbase64にして埋め込む
+async function replaceUrlToBase64(cssText) {
+  const regex = /url\s*\(["']*(.+?)["']*\)/g;
+  const promises = [];
+  cssText.replace(regex, async (match, url) => {
+    const promise = fetchFileBase64(url)
+      .then(base64 => `url(${base64})`);
+    promises.push(promise);
   });
-});
+  const data = await Promise.all(promises);
+  return cssText.replace(regex, () => data.shift());
+}
+
+// ファイルを取得してbase64で返す
+async function fetchFileBase64(fileUrl) {
+  const req = new Request(fileUrl);
+  let mimeType = "";
+  const res = await fetch(req);
+  if (!res.ok) {
+    throw new Error(`fetch error ${res.status}`);
+  }
+  mimeType = res.headers.get("content-type").split(";")[0];
+  const aBuff = await res.arrayBuffer();
+  let binary = "";
+  const bytes = new Uint8Array(aBuff);
+  const length = bytes.byteLength;
+  for (let i = 0; i < length; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return `data:${mimeType};base64,${window.btoa(binary)}`;
+}
+
+// 画像をbase64にして返す
+function imageToBase64($img, mimeType="image/png") {
+  const $canvas = document.createElement("canvas");
+  $canvas.width  = $img.naturalWidth;
+  $canvas.height = $img.naturalHeight;
+  const ctx = $canvas.getContext("2d");
+  ctx.drawImage($img, 0, 0);
+  return $canvas.toDataURL(mimeType);
+}
+
+// 対象要素をSVGにして返す
+function elementToSvgText($target, cssText) {
+  const $clone = $target.cloneNode(true);
+
+  const $targetImages = $target.querySelectorAll("img");
+  const $cloneImages = $clone.querySelectorAll("img");
+
+  for (let i=0; i < $targetImages.length; i++) {
+    $cloneImages[i].src = imageToBase64($targetImages[i]);
+  }
+
+  const clientRect = $target.getBoundingClientRect();
+  const width = clientRect.width;
+  const height = clientRect.height;
+  console.log(width, height);
+  const xmlText = new XMLSerializer().serializeToString($clone);
+  return `
+    <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
+      <foreignObject width="100%" height="100%" requiredExtensions="http://www.w3.org/1999/xhtml">
+        <div xmlns="http://www.w3.org/1999/xhtml">
+          <style>
+            ${cssText}
+          </style>
+          ${xmlText}
+        </div>
+      </foreignObject>
+    </svg>
+  `;
+}
 
 
 
