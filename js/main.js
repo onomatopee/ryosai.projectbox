e7acb22341e61ddfdb520f07971c8f9be08d5c44
Shinnosuke Shoda <raindrops@vivaldi.net>
2024-10-26 02:39:35 +0900

    fixed bug

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function generateImages() {
   for (let i = 1; i <= 10; i++) {
     routine(String(i)+".jpg",String(i),"雪国","ああああああ！","img/"+String(i)+".jpg");
     start();
-    saveCanvasStr(nm);
+    saveCanvasStr(String(i)+".jpg");
   }
 }
 
